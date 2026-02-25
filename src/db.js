24494a5c74705333f8a519dea50087f00af64986
Fajar Teguh Permana <fajarcode21@gmail.com>
2026-02-25 20:19:21 +0700

    fix some bugs

--- src/db.js
+++ src/db.js
@@ -2,11 +2,8 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.IS_PROD === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
